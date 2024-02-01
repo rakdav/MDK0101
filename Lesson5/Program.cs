d36723d1a527f7cafe09133c16137008ee6eeb54
Gogs <gogs@fake.local>
2024-02-01 15:09:10 +0200

    Lesson5

---
+++ Lesson5/Program.cs
@@ -0,0 +1,39 @@
+﻿Counter counter1 = new Counter { Value = 6 };
+Counter counter2 = new Counter { Value = 8 };
+Console.WriteLine(counter1>counter2);
+Console.WriteLine(counter1+counter2);
+Console.WriteLine(counter1++);
+Console.WriteLine(counter2--);
+class Counter
+{
+    public int Value { get; set; }
+    public static bool operator > (Counter c1,Counter c2)
+    {
+        return c1.Value > c2.Value;
+    }
+    public static bool operator <(Counter c1, Counter c2)
+    {
+        return c1.Value < c2.Value;
+    }
+    public static Counter operator + (Counter c1, Counter c2)
+    {
+        return new Counter { Value = c1.Value + c2.Value };
+    }
+    public static Counter operator - (Counter c1, Counter c2)
+    {
+        return new Counter { Value = c1.Value - c2.Value };
+    }
+    public override string? ToString()
+    {
+        return Value.ToString();
+    }
+    public static Counter operator ++(Counter c1)
+    {
+        return new Counter { Value = c1.Value++ };
+    }
+    public static Counter operator --(Counter c1)
+    {
+        return new Counter { Value = c1.Value-- };
+    }
+}
+
