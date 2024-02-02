6c68a8975510c1a7fe33b5d2a9b2ac23beb182e8
Gogs <gogs@fake.local>
2024-02-02 10:11:04 +0200

    Lesson6

---
+++ Lesson6/Program.cs
@@ -0,0 +1,34 @@
+﻿Company samsung = new Company(new[]
+{
+    new Person("Vasia"), new Person("Arina"), new Person("Masha"),
+    new Person("Boris")
+});
+Person Vasia = samsung[2];
+Console.WriteLine(Vasia.Name);
+class Person
+{
+    public string? Name { get; }
+    public Person(string name){ Name = name;}
+}
+class Company
+{
+    Person[] personal;
+    public Company(Person[] personal)
+    {
+        this.personal = personal;
+    }
+    public Person this[int index]
+    {
+        get 
+        {
+            if (index >= 0 && index < personal.Length)
+                return personal[index];
+            else throw new ArgumentOutOfRangeException();
+        }
+        set
+        {
+            if (index >= 0 && index < personal.Length)
+                personal[index] = value;
+        }
+    }
+}
