b73b72b2f3f2ab0c7b2b4bbfa517408ac5a77ccf
Gogs <gogs@fake.local>
2024-02-12 10:38:08 +0200

    lab4

---
+++ Lab4/Monster.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Monster<T>:ICloneable,IComparable
+    {
+        public T? id { get; set; }
+        public string? Name { get; set; }
+        public int Health { get; set; }
+        public int Ammo { get; set; }
+        public object Clone()
+        {
+            return new Monster<T>()
+            {
+                id=this.id,
+                Name=this.Name,
+                Health=this.Health,
+                Ammo=this.Ammo
+            };
+        }
+        public int CompareTo(object? obj)
+        {
+            if(obj is Monster<T>)
+            {
+                Monster<T>? monster = obj as Monster<T>;
+                return this.Health.CompareTo(monster!.Health);
+            }
+            else throw new Exception("Невозможно сравнить два объекта");
+        }
+    }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+
+string[] people = { "Vasya", "Petya", "Sam", "Jerry" };
+IEnumerator peoplesEnumerator = people.GetEnumerator();
+while (peoplesEnumerator.MoveNext())
+{
+    string item = (string)peoplesEnumerator.Current;
+    Console.Write(item+" ");
+}
+peoplesEnumerator.Reset();
+Console.WriteLine();
+Week week = new Week();
+foreach(var day in week)
+{
+    Console.Write(day+" ");
+}
+class Week : IEnumerable
+{
+    string[] days = { "Sun", "Mon", "Thu", "Wed", "Ther", "Fri", "Sat" };
+    public IEnumerator GetEnumerator()
+    {
+        //return days.GetEnumerator();
+        return new WeekEnumerator(days);
+    }
+}
+class WeekEnumerator : IEnumerator
+{
+    string[] days;
+    int position = -1;
+    public WeekEnumerator(string[] days)
+    {
+        this.days = days;
+    }
+    public object Current
+    {
+        get
+        {
+            if (position == -1 || position >= days.Length)
+                throw new ArgumentException();
+            return days[position];
+        }
+    }
+    public bool MoveNext()
+    {
+        if (position < days.Length - 1)
+        {
+            position++;
+            return true;
+        }
+        else return false;
+    }
+    public void Reset()
+    {
+        position=-1;
+    }
+}
+
