3f85a7fabbb58723cfd6a5df54ae4d4c375362da
Gogs <gogs@fake.local>
2024-02-16 11:35:13 +0200

    Lab4

--- Lab4/MainWindow.xaml.cs
+++ Lab4/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -17,6 +18,8 @@ namespace Lab4
     public partial class MainWindow : Window
     {
         public Monster<int>[]? Monsters { get; set; } = null;
+        private bool isAmmoSort = true;
+        private bool isHealthSort = true;
         public MainWindow()
         {
             InitializeComponent();
@@ -28,20 +31,91 @@ namespace Lab4
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Monsters = null;
-            Monsters = new Monster<int>[int.Parse(tbCount.Text)];
-            MessageBox.Show("Массив размером "+Monsters.Length+" элемента создан");
-            mmCopy.IsEnabled = true;
-            mmCreate.IsEnabled = true;
-            tbCreate.IsEnabled = true;
-            tbCopy.IsEnabled = true;
-            monsterList.ItemsSource = null;
-            stCount.Content = "Создано " + Monster<int>.Count + " из " + tbCount.Text;
+            if (tbCount.Text.Length != 0)
+            {
+                Monsters = null;
+                Monsters = new Monster<int>[int.Parse(tbCount.Text)];
+                MessageBox.Show("Массив размером " + Monsters.Length + " элемента создан");
+                Monster<int>.Count = 0;
+                mmCopy.IsEnabled = true;
+                mmCreate.IsEnabled = true;
+                tbCreate.IsEnabled = true;
+                tbCopy.IsEnabled = true;
+                monsterList.ItemsSource = null;
+                stCount.Content = "Создано " + Monster<int>.Count + " из " + tbCount.Text;
+            }
+            else
+            {
+                MessageBox.Show("Введите размер массива");
+            }
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            if (Monsters!.Length>Monster<int>.Count)
+            Add();
+        }
+
+        private void mmCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Copy();
+        }
+        void GridViewColumnHeaderClickedHandler(object sender,
+                                        RoutedEventArgs e)
+        {
+            var headerClicked = e.OriginalSource as GridViewColumnHeader;
+            if(headerClicked!.Content.ToString()=="Здоровье")
+            {
+                if (isHealthSort)
+                {
+                    Array.Sort(Monsters!);
+                }
+                else
+                {
+                    Monsters=Monsters!.OrderByDescending(p => p.Health).ToArray();
+                }
+                isHealthSort = !isHealthSort;
+            }   
+            else if(headerClicked.Content.ToString() == "Патроны")
+            {
+                if (isAmmoSort)
+                {
+                    Array.Sort(Monsters!, new AmmoComparator());    
+                }
+                else
+                {
+                    Monsters = Monsters!.OrderByDescending(p => p.Ammo).ToArray();
+                }
+                isAmmoSort = !isAmmoSort;
+            }
+            monsterList.ItemsSource = null;
+            monsterList.ItemsSource = Monsters;
+        }
+
+        private void tbCopy_Click(object sender, RoutedEventArgs e)
+        {
+            Copy();
+        }
+
+        private void tbCreate_Click(object sender, RoutedEventArgs e)
+        {
+            Add();
+        }
+
+        private void Copy()
+        {
+            if (monsterList.SelectedItems.Count != 0)
+            {
+                Monster<int>? monster = monsterList.SelectedItem as Monster<int>;
+                Monsters![Monster<int>.Count] = (Monster<int>)monster!.Clone();
+                monsterList.ItemsSource = null;
+                monsterList.ItemsSource = Monsters;
+                Monster<int>.Count++;
+                stCount.Content = "Создано " + Monster<int>.Count + " из " + tbCount.Text;
+            }
+        }
+        private void Add()
+        {
+            if (Monsters!.Length > Monster<int>.Count)
             {
                 AddMonster add = new AddMonster();
                 if (add.ShowDialog() == true)
@@ -55,7 +129,7 @@ namespace Lab4
                 }
                 monsterList.ItemsSource = null;
                 monsterList.ItemsSource = Monsters;
-                stCount.Content = "Создано " + Monster<int>.Count+" из "+ tbCount.Text;
+                stCount.Content = "Создано " + Monster<int>.Count + " из " + tbCount.Text;
             }
             else
             {
@@ -63,17 +137,15 @@ namespace Lab4
             }
         }
 
-        private void mmCopy_Click(object sender, RoutedEventArgs e)
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
         {
-            if (monsterList.SelectedItems.Count != 0)
-            {
-                Monster<int>? monster=monsterList.SelectedItem as Monster<int>;
-                Monsters![Monster<int>.Count] = (Monster<int>)monster!.Clone();
-                monsterList.ItemsSource = null;
-                monsterList.ItemsSource = Monsters;
-                Monster<int>.Count++;
-                stCount.Content = "Создано " + Monster<int>.Count + " из " + tbCount.Text;
-            }
+            this.Close();
+        }
+
+        private void MenuItem_Click_2(object sender, RoutedEventArgs e)
+        {
+            AboutWindow about = new AboutWindow();
+            about.ShowDialog();
         }
     }
 }
