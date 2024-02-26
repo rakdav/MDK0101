20b9909848905eb52fc7999a0b2e1759e3ce7ea0
Gogs <gogs@fake.local>
2024-02-26 11:03:57 +0200

    Lab5last

--- Lab5/MainWindow.xaml.cs
+++ Lab5/MainWindow.xaml.cs
@@ -20,5 +20,90 @@ namespace Lab5
         {
             InitializeComponent();
         }
+
+        private void Button_Click ( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                double a = double.Parse(SideA.Text);
+                double b = double.Parse(SideB.Text);
+                double c = double.Parse(SideC.Text);
+                double x = ( a - b ) / 2;
+                double h = Math.Sqrt(c*c-x*x);
+                double s = ( a - b ) * h / 2;
+                Result.Text = $"Площадь трапеции {s:F2}";
+            }
+            catch
+            {
+                MessageBox.Show("Проверьте правильность введенных данных");
+            }
+        }
+
+        private void Button_Click_1 ( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                double a = double.Parse(SideA.Text);
+                if (a == 0) throw new Exception("Введите сторону A отличную от нуля");
+                double b = double.Parse(SideB.Text);
+                if (b == 0) throw new Exception("Введите сторону B отличную от нуля");
+                double c = double.Parse(SideC.Text);
+                if (c == 0) throw new Exception("Введите сторону C отличную от нуля");
+                double x = ( a - b ) / 2;
+                double h = Math.Sqrt(c * c - x * x);
+                double s = ( a - b ) * h / 2;
+                Result.Text = $"Площадь трапеции {s:F2}";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void Button_Click_2 ( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                double a = double.Parse(SideA.Text);
+                if (a == 0) throw new Exception("Введите сторону A отличную от нуля");
+                double b = double.Parse(SideB.Text);
+                if (b == 0) throw new Exception("Введите сторону B отличную от нуля");
+                double c = double.Parse(SideC.Text);
+                if (c == 0) throw new Exception("Введите сторону C отличную от нуля");
+                double x = ( a - b ) / 2;
+                double h = Math.Sqrt(c * c - x * x);
+                double s = ( a - b ) * h / 2;
+                Result.Text = $"Площадь трапеции {s:F2}";
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void Button_Click_3 ( object sender, RoutedEventArgs e )
+        {
+            try
+            {
+                double a = double.Parse(SideA.Text);
+                if (a == 0) throw new TrapeciaExeption("Введите сторону A отличную от нуля");
+                double b = double.Parse(SideB.Text);
+                if (b == 0) throw new TrapeciaExeption("Введите сторону B отличную от нуля");
+                double c = double.Parse(SideC.Text);
+                if (c == 0) throw new TrapeciaExeption("Введите сторону C отличную от нуля");
+                double x = ( a - b ) / 2;
+                double h = Math.Sqrt(c * c - x * x);
+                double s = ( a - b ) * h / 2;
+                Result.Text = $"Площадь трапеции {s:F2}";
+            }
+            catch (TrapeciaExeption ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
     }
 }
