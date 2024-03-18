11b985518eab20f869da60d4616badcce52baa5b
Gogs <gogs@fake.local>
2024-03-18 11:32:44 +0200

    lesson14-last

--- Lesson14/Program.cs
+++ Lesson14/Program.cs
@@ -31,6 +31,8 @@ foreach (int item in list1)
     Console.Write(item + " ");
 }
 Console.WriteLine();
+list2.RemoveAll(p=>p%2==0);
+list2.RemoveRange(0, 2);
 list2.Clear();
 //размеры
 Console.WriteLine(list1.Count);
@@ -59,4 +61,29 @@ Console.WriteLine();
 Console.WriteLine(list1.Contains(5));
 Console.WriteLine(list1.IndexOf(5));
 Console.WriteLine(list1.LastIndexOf(2));
+Console.WriteLine(list1.BinarySearch(5));//бинарный поиск - только в упорядоченном массиве
 
+var people = new List<string>() { "Маша", "Боря", "Дудка", "Леша","Том" };
+Console.WriteLine(people.Exists(p=>p.Length==3));
+Console.WriteLine(people.Find(p=>p.Length==4));//первый элемент
+Console.WriteLine(people.FindLast(p=>p.Length==4));//последний элемент
+var peopleWithLength3 = people.FindAll(p => p.Length == 4);
+foreach (var item in peopleWithLength3)
+{
+    Console.Write(item+" ");
+}
+Console.WriteLine();
+//получение диапазона и копирование в массив
+var range = people.GetRange(1,3);
+foreach (var item in range)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+string[] partOfPeople = new string[3];
+people.CopyTo(0, partOfPeople, 0, 3);
+foreach (var item in partOfPeople)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
