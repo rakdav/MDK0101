e83582d3f9f886df2225f3488c4ed3b15c3a7b8b
Gogs <gogs@fake.local>
2024-03-22 10:38:16 +0200

    ProjectCollection

---
+++ ProjectCollections/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectCollections
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<string> people = new List<string>{ "Маша","Дудка","Леша"};
+        public MainWindow ()
+        {
+            InitializeComponent();
+            people.Add("Боря");
+            lbPeople.ItemsSource = people;
+        }
+    }
+}
