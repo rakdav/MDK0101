6e39bb115196ac18e3f4f6e4ba4d4b655618c7cc
Gogs <gogs@fake.local>
2024-04-05 10:39:13 +0200

    lab6

--- Lab6/MainWindow.xaml.cs
+++ Lab6/MainWindow.xaml.cs
@@ -16,9 +16,37 @@ namespace Lab6
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<int> listLab1;
         public MainWindow ()
         {
             InitializeComponent();
+            listLab1 = new List<int>();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            listLab1.Add(int.Parse(tbElement.Text));
+            lbList.ItemsSource = null;
+            lbList.ItemsSource = listLab1;
+            tbElement.Text = "";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            int index = lbList.SelectedIndex;
+            listLab1.RemoveAt(index);
+            lbList.ItemsSource = null;
+            lbList.ItemsSource = listLab1;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            int count = 0;
+            foreach(int l in listLab1)
+            {
+                if (l < 0) count++;
+            }
+            Result.Text ="Количество отрицательных:"+count.ToString();
         }
     }
 }
