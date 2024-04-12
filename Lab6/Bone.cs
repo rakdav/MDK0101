abdd2349588bf1ba18edc6724fd8bec1ac947aa6
Gogs <gogs@fake.local>
2024-04-12 09:50:48 +0200

    Lab9

---
+++ Lab6/Bone.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Bone
+    {
+        public int Number { get; set; }
+        public int First { get; set; }
+        public int Second { get; set; }
+        public int Summa { get; set; }
+
+        public Bone(int number)
+        {
+            Random random = new Random();
+            Number = number;
+            First = random.Next(1, 7);
+            Second = random.Next(1, 7);
+            Summa = First + Second;
+        }
+    }
+}
