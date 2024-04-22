d7a148a8547057a8bbcd1a357162547ccba0eb3b
Gogs <gogs@fake.local>
2024-04-22 14:36:27 +0200

    Lab10-2

--- Lab10-11/Lab10.xaml.cs
+++ Lab10-11/Lab10.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Lab10_11
 {
@@ -23,6 +26,42 @@ namespace Lab10_11
         public Lab10()
         {
             InitializeComponent();
+        }
+        private void Create()
+        {
+            tbEdit.Text = "";
+        }
+        private async void Open()
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files(*.txt)|*.txt";
+            if (ofd.ShowDialog() == true)
+            {
+                using (StreamReader reader = new StreamReader(ofd.FileName))
+                {
+                    tbEdit.Text = await reader.ReadToEndAsync();
+                }
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Create();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            Open();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Open();
         }
     }
 }
