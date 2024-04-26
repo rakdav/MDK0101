22049408aed01cd728d19e7d6f0bb710536e1860
Gogs <gogs@fake.local>
2024-04-26 15:52:29 +0200

    Lab11

---
+++ Lab10-11/Film.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_11
+{
+    enum Days 
+    {
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+    class Film
+    {
+        public string? Name { get; set; }
+        public int CountSeria { get; set; }
+        public int Duration { get; set; }
+        public List<Days>? Day { get; set; }
+        public int getPeriod() => Duration * CountSeria;
+    }
+}
