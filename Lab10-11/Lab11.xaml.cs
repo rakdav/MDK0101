9adb5dd58094d95cfc933f26899fd9b13649af65
Gogs <gogs@fake.local>
2024-05-03 15:35:38 +0200

    Lab11

--- Lab10-11/Lab11.xaml.cs
+++ Lab10-11/Lab11.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -49,16 +53,66 @@ namespace Lab10_11
         }
         public void UpdateUI()
         {
+            NameFilm.Text = "";
+            CountFilm.Text = "";
+            DurationFilm.Text = "";
+            Sun.IsChecked = false;
+            Mon.IsChecked = false;
+            Thu.IsChecked = false;
+            Wen.IsChecked = false;
+            Thi.IsChecked = false;
+            Fri.IsChecked = false;
+            Sat.IsChecked = false;
             FilmsPanel.Children.Clear();
             foreach(Film i in films)
             {
                 FilmsPanel.Children.Add(new FilmControl(i));
             }
+            maxFilm.FontSize = 24;
+            maxFilm.Content = MaxFilm().Name+":"+MaxFilm().getPeriod() + " мин";
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Бинарные файлы(*.dat)|*.dat";
+           
+            if (sfd.ShowDialog() == true)
+            {
+                using (BinaryWriter writer = new BinaryWriter(File.Open(sfd.FileName, FileMode.OpenOrCreate)))
+                {
+                    string json = JsonSerializer.Serialize(films);
+                    writer.Write(json);
+                }
+            }
+        }
 
+        private Film MaxFilm()
+        {
+            Film max = films[0];
+            foreach (Film f in films)
+            {
+                if (f.getPeriod() > max.getPeriod()) max = f;
+            }
+            return max;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Бинарные файлы(*.dat)|*.dat";
+            if (ofd.ShowDialog() == true)
+            {
+                using (BinaryReader reader = new BinaryReader(File.Open(ofd.FileName, FileMode.Open)))
+                {
+                    string json = reader.ReadString();
+                    List<Film> desFilms= JsonSerializer.Deserialize<List<Film>>(json)!;
+                    foreach(Film f in desFilms)
+                    {
+                        films.Add(f);
+                    }
+                    UpdateUI();
+                }
+            }
         }
     }
 }
