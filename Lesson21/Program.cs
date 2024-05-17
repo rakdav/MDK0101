2e18a016aa0d50305782bf40ab11c4ff353d8370
Gogs <gogs@fake.local>
2024-05-17 12:33:39 +0200

    Lesson21

---
+++ Lesson21/Program.cs
@@ -0,0 +1,16 @@
+﻿class Player
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Position { get; set; }
+    public int Age { get; set; }
+    public int? TeamId { get; set; }
+    public Team? Team { get; set; }
+}
+class Team
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Coach { get; set; }
+    public ICollection<Player> Players { get; set; }
+}
